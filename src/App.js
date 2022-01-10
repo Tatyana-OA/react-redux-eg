e213c35d9e7355177f034584308f5a4456b6880c
Tatyana Asenova <tasenova@devrix.com>
2022-01-10 17:42:07 +0200

    Adding actions to App and using them via dispatch on click event for each button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import {useSelector} from 'react-redux';
+import { useDispatch } from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
 
 	// useSelector gives access to the whole state
 	const counter = useSelector( state => state.counter)
 	const isLogged = useSelector( state => state.isLogged )
+	const dispatch = useDispatch();
   return (
     <div className="App">
 		<h1>Tanya's Redux Example</h1>
 		<span>Counter is at: {counter}</span>
 		<div>
-		<button>INCREMENT + </button>
+		<button onClick = {() => dispatch(increment())}>INCREMENT + </button>
 		</div>
 		<div>
-		<button>DECREMENT - </button>
+		<button onClick = {() => dispatch(decrement())} >DECREMENT - </button>
 		</div>
 		<div>Authentication status: {isLogged ? 'Logged in' : 'Not logged in'}</div>
     </div>
