a2339390961a9cd611c9860911cef90e64901340
Tatyana Asenova <tasenova@devrix.com>
2022-01-10 17:37:50 +0200

    Using useSelector to access state and display it in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import {useSelector} from 'react-redux';
 
 function App() {
+
+	// useSelector gives access to the whole state
+	const counter = useSelector( state => state.counter)
+	const isLogged = useSelector( state => state.isLogged )
   return (
     <div className="App">
 		<h1>Tanya's Redux Example</h1>
+		<span>Counter is at: {counter}</span>
+		<div>
+		<button>INCREMENT + </button>
+		</div>
+		<div>
+		<button>DECREMENT - </button>
+		</div>
+		<div>Authentication status: {isLogged ? 'Logged in' : 'Not logged in'}</div>
     </div>
   );
 }
