1c12f91ce2f7e914c5deda33daa48df6e5ca642f
Tatyana Asenova <tasenova@devrix.com>
2022-01-10 16:58:37 +0200

    Initial Redux example

--- src/index.js
+++ src/index.js
@@ -2,6 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+
+
+// STORE-> globalized state, holding the data used throughout the whole application
+
+// ACTION -> Describes what we want to do with regard to the state; A function which returns an object
+
+const increment = () => {
+	return {
+		type: 'INCREMENT',
+	}
+}
+const decrement = () => {
+	return {
+		type: 'DECREMENT',
+	}
+}
+
+// REDUCER -> Describes how the actions transform the state into the next state ; modifies store based on the chosen action
+
+const counter = (state = 0, action) => {
+	switch(action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+	}
+}
+
+
+let store = createStore(counter)
+
+//Display in console
+
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> Trigger. Action is dispatched to the reducer, reducer determines how to affect the state and then acts on it.
+
+store.dispatch(increment())
 
 
 ReactDOM.render(
@@ -11,7 +50,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
