1269c7364697e96f04913fd4452e30c953c7c1b1
Tatyana Asenova <tasenova@devrix.com>
2022-01-10 17:15:33 +0200

    Combining the reducers and adding them to index.js of app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore} from 'redux';
+import allReducers from './reducers';
 
 
-
+const store = createStore(allReducers)
 
 ReactDOM.render(
   <React.StrictMode>
