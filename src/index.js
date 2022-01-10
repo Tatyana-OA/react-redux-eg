ccacbde5c9b9247750a4fbb500df12f121834caa
Tatyana Asenova <tasenova@devrix.com>
2022-01-10 17:32:09 +0200

    Encapsulating app with the redux provider which allows access to the store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from './App';
 import {createStore} from 'redux';
 import allReducers from './reducers';
 
+import {Provider} from 'react-redux';
+
 // Adding combined reducers to store + ability to use Redux devtools with chrome
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
+	  <Provider store={store}>
     <App />
+	</Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
